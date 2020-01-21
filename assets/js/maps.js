58baa41458cf4649e7a875b2963ef1d9237cfa20
Joe Sudron <joesudron@outlook.com>
2020-01-21 22:26:01 +0000

    fixd the search function zooming in on the searched city

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,46 +1,45 @@
-// This example uses the autocomplete feature of the Google Places API.
-// It allows the user to find all hotels in a given place, within a given
-// country. It then displays markers for all the hotels returned,
-// with on-click details for each hotel.
+// This document allows the user to find accommodation, bars/cafes/restaurants, public
+// transport, takeaways and tourist attractions in a given city.
+// It then displays markers for all the places returned, with on-click details
+// for each set of filter parameters.
 
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+var map, places, infoWindow;
+var markers = [];
+var autocomplete;
+var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
+var hostnameRegexp = new RegExp('^https?://.+?/');
 
-    var map, places, infoWindow;
-    var markers = [];
-    var autocomplete;
-    var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
-    var hostnameRegexp = new RegExp('^https?://.+?/');
 
-    var countries = {
-        'uk': {             // For centering the map as opposed to searching
-            center: {lat: 64.8, lng: 2.6},
-            zoom: 5
-        }
-    };
-
-    // Resets the input fields and map.
-    function reset() {
-        clearResults();
-        clearMarkers();
-        $('#category')[0].selectedIndex = 0;
-        $('#autocomplete').val("");
-        $('#results-heading').html("");
-        map.setZoom(countries.uk.zoom);
-        map.setCenter(countries.uk.center);
-        place = "";
+var countries = {
+    'uk': {                     //For centering the map as opposed to searching.
+        center: {lat: 62.8, lng: 2.6},
+        zoom: 4
     }
+};
 
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: countries['uk'].zoom,
-          center: countries['uk'].center,
-          mapTypeControl: false,
-          panControl: false,
-          zoomControl: false,
-          streetViewControl: false,
-          styles: [
+
+//Resets the map and input fields.
+function reset() {
+    clearResults();
+    clearMarkers();
+    $('#category')[0].selectedIndex = 0;
+    $("#autocomplete").val("");
+    $('#results-heading').html("");
+    $('#hr').hide();
+    map.setZoom(countries.uk.zoom);
+    map.setCenter(countries.uk.center);
+    place = "";
+}
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: countries['uk'].zoom,
+        center: countries['uk'].center,
+        mapTypeControl: false,
+        panControl: false,
+        streetViewControl: false,
+        styles: [
             {
                 "featureType": "administrative",
                 "elementType": "labels.text.fill",
@@ -122,30 +121,37 @@
         ]
     });
 
+
     infoWindow = new google.maps.InfoWindow({
         content: document.getElementById('info-content')
     });
 
+
     // Create the autocomplete object and associate it with the UI input control.
-    // Restrict the search to the default country, and to place type "cities".
+    // Place type "cities".
     autocomplete = new google.maps.places.Autocomplete(
-        /** @type {!HTMLInputElement} */ 
-        (document.getElementById('autocomplete')), {
-              types: ['(cities)'],
-              componentRestrictions: {country: ['dk', 'fi', 'is', 'no', 'se']}
+        /** @type {!HTMLInputElement} */
+        (
+            document.getElementById('autocomplete')), {
+            types: ['(cities)'],
+            componentRestrictions: { country: ['dk', 'fi', 'is', 'no', 'se']}
         });
     places = new google.maps.places.PlacesService(map);
-    
-    // DOM event listener
+
+
+    // Event listeners.
     autocomplete.addListener('place_changed', onPlaceChanged);
     document.getElementById('category').addEventListener('change', onPlaceChanged);
+
+    $('#hr').hide();
 }
+
 
 // When the user selects a city, get the place details for the city and
 // zoom the map in on the city.
 function onPlaceChanged() {
     var place = autocomplete.getPlace();
-    if ($('accomodation').is('selected')) {
+    if ($("#accommodation").is(':selected')) {
         if (place.geometry) {
             map.panTo(place.geometry.location);
             map.setZoom(14);
@@ -156,49 +162,58 @@ function onPlaceChanged() {
             doNearbySearch(search);
         }
         else {
-            $('#autocomplete').attr('placeholder', 'Enter a town or city');
+            $('#autocomplete').attr("placeholder", "Enter a town or city");
         }
     }
 }
 
-function doNearbySearch(search) {
-    places.doNearbySearch(search, function(results, status) {
 
-        if(status == google.maps.places.PlacesServiceStatus.OK) {
+function doNearbySearch(search) {
+
+    places.nearbySearch(search, function(results, status) {
+
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
             clearResults();
             clearMarkers();
-            document.getElementById('results-heading').innerHTML = 'Results';
+            document.getElementById('results-heading').innerHTML = "Results";
+            $('#hr').show();
 
-            // Create a marker for each location found, and assign a letter of the alphabetic to each marker icon.
+
+            // Create a marker for each place found, and add letter.
             for (var i = 0; i < results.length; i++) {
-              var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-              var markerIcon = MARKER_PATH + markerLetter + '.png';
+                var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+                var markerIcon = MARKER_PATH + markerLetter + '.png';
 
-              // Use marker animation to drop the icons incrementally on the map.
-              markers[i] = new google.maps.Marker({
-                position: results[i].geometry.location,
-                animation: google.maps.Animation.DROP,
-                icon: markerIcon
-              });
 
-              // If the user clicks a marker, show the details of that hotel in an info window.
-              markers[i].placeResult = results[i];
-              google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-              setTimeout(dropMarker(i), i * 100);
-              addResult(results[i], i);
+                // Use marker animation to drop the icons incrementally on the map.
+                markers[i] = new google.maps.Marker({
+                    position: results[i].geometry.location,
+                    animation: google.maps.Animation.DROP,
+                    icon: markerIcon
+                });
+
+
+                // If the user clicks a marker, show the details of that place
+                // in an info window.
+                markers[i].placeResult = results[i];
+                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+                setTimeout(dropMarker(i), i * 100);
+                addResult(results[i], i);
             }
         }
     });
 }
 
+
 function clearMarkers() {
     for (var i = 0; i < markers.length; i++) {
         if (markers[i]) {
-        markers[i].setMap(null);          
+            markers[i].setMap(null);
         }
     }
     markers = [];
 }
+
 
 function dropMarker(i) {
     return function() {
@@ -206,23 +221,28 @@ function dropMarker(i) {
     };
 }
 
-// Add a results table in results section
-function addResults(results, i) {
+
+// Adds a results table into the results section
+function addResult(result, i) {
     var results = document.getElementById('results');
     var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
     var markerIcon = MARKER_PATH + markerLetter + '.png';
 
+
     // Creates striped effect in results table
     var tr = document.createElement('tr');
-    tr.style.backgroundColor = (i % 2 === 0 ? '#f0f0f0' : '#ffffff');
+    tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
     tr.onclick = function() {
-        google.map.event.trigger(markers[i], 'click');
+        google.maps.event.trigger(markers[i], 'click');
 
-        // Scrolls to the location on map when search result clicked
+
+        // Scrolls to the map section when you click on a search result
         $([document.documentElement, document.body]).animate({
-            scrollTop: $('map').offset().top
+            scrollTop: $("#map").offset().top
         }, 500);
+
     };
+
 
     var iconTd = document.createElement('td');
     var nameTd = document.createElement('td');
@@ -233,46 +253,52 @@ function addResults(results, i) {
     var name = document.createTextNode(result.name);
     iconTd.appendChild(icon);
     nameTd.appendChild(name);
-    tr.appendChild(icon);
-    tr.appendChild(name);
     tr.appendChild(iconTd);
     tr.appendChild(nameTd);
     results.appendChild(tr);
 }
 
+
 function clearResults() {
     var results = document.getElementById('results');
-    while (results.childNodes [0]) {
+    while (results.childNodes[0]) {
         results.removeChild(results.childNodes[0]);
     }
 }
 
-// Anchor the info window for the selected search result to the marker for that selected search result
+
+// Get the place details for each search result. Show the information in an info
+// window, anchored on the marker for the place that the user selected.
 function showInfoWindow() {
     var marker = this;
     places.getDetails({ placeId: marker.placeResult.place_id },
         function(place, status) {
             if (status !== google.maps.places.PlacesServiceStatus.OK) {
                 return;
-        }
-        infoWindow.open(map, marker);
-        buildIWContent(place);
-    });
+            }
+            infoWindow.open(map, marker);
+            buildIWContent(place);
+        });
 }
 
-// Load the place info into HTML elements within the info window.
+
+// Load the place information into the HTML elements used by the info window.
 function buildIWContent(place) {
-    document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' + 'src="' + place.icon + '">' + place.icon + '"/>';
-    document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url + '">' + place.name + '</a></b>';
+    document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
+        'src="' + place.icon + '"/>';
+    document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
+        '">' + place.name + '</a></b>';
     document.getElementById('iw-address').textContent = place.vicinity;
 
     if (place.formatted_phone_number) {
         document.getElementById('iw-phone-row').style.display = '';
-        document.getElementById('iw-phone').textContent = place.formatted_phone_number;
+        document.getElementById('iw-phone').textContent =
+            place.formatted_phone_number;
     }
     else {
         document.getElementById('iw-phone-row').style.display = 'none';
     }
+
 
     // Assign a five-star rating to the hotel, using a black star ('&#10029;')
     // to indicate the rating the hotel has earned, and a white star ('&#10025;')
