4ea769f77f97dd3263937b9ae56f605cf1c5f8a6
Joe Sudron <joesudron@outlook.com>
2020-01-23 15:56:53 +0000

    added the sendEmail javascript file & links on contact.html

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("outlook", "joe", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "query": contactForm.query.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
